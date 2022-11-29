e7166f5275b8a6dc8ec1006324ad6490534e8408
unknown <adonamarkkevin@gmail.com>
2022-11-30 03:07:17 +0800

    done with queue

--- src/controller/queue.controller.ts
+++ src/controller/queue.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { Queue } from "../entity/queue.entity";
+import { QueueInternal } from "../entity/queue_internal.entity";
+import { assignDoctor } from "../service/transaction_services.service";
 
 export const viewAllQueue = async (req: Request, res: Response) => {
     try {
@@ -20,6 +22,9 @@ export const viewAllQueue = async (req: Request, res: Response) => {
                         id: parseInt(clinic.id),
                     },
                 },
+            },
+            order: {
+                created_at: "DESC",
             },
             relations: [
                 "transaction_info",
@@ -49,6 +54,110 @@ export const clearQueue = async (req: Request, res: Response) => {
         return res.send({
             status: "Success",
             message: "Queue Cleared",
+        });
+    } catch (error) {
+        return res.status(401).send({
+            status: `Server Error`,
+            message: `Please contact administrator`,
+            error: error.message,
+        });
+    }
+};
+
+export const viewAllInternalQ = async (req: Request, res: Response) => {
+    try {
+        const userFound = req.user;
+        const clinic = userFound.clinic;
+        if (!clinic) {
+            return res.status(401).send({
+                status: "Unauthorized",
+                message:
+                    "Permission denied. Either you don't have access to this resources or you are log in as super admin",
+            });
+        }
+
+        let [allQ, count] = await QueueInternal.findAndCount({
+            where: {
+                transaction_service: {
+                    transaction_info: {
+                        clinic: {
+                            id: clinic.id,
+                        },
+                    },
+                },
+            },
+            order: {
+                created_at: "DESC",
+            },
+            relations: [
+                "transaction_service",
+                "transaction_service.assigned_doctor",
+            ],
+        });
+
+        return res.send({
+            data: allQ,
+            count: count,
+        });
+    } catch (error) {
+        return res.status(401).send({
+            status: `Server Error`,
+            message: `Please contact administrator`,
+            error: error.message,
+        });
+    }
+};
+
+export const clearAllInternalQ = async (req: Request, res: Response) => {
+    try {
+        await QueueInternal.clear();
+
+        return res.send({
+            status: "Success",
+            message: "Queue Cleared",
+        });
+    } catch (error) {
+        return res.status(401).send({
+            status: `Server Error`,
+            message: `Please contact administrator`,
+            error: error.message,
+        });
+    }
+};
+
+export const assignDoctorOnQ = async (req: Request, res: Response) => {
+    try {
+        const { queue_id } = req.params;
+        const userFound = req.user;
+        const department = userFound.department;
+        const queueFound = await QueueInternal.findOne({
+            where: {
+                id: parseInt(queue_id),
+            },
+            relations: ["transaction_service"],
+        });
+
+        if (department.id !== 12) {
+            return res.status(403).send({
+                status: "Bad Request",
+                message: "Doctor's module: You aren't signed in as a Doctor",
+            });
+        }
+
+        if (!queueFound) {
+            return res.status(404).send({
+                status: "Not Found",
+                message: "Queue does not exist",
+            });
+        }
+
+        const serviceId = queueFound.transaction_service.id;
+        await assignDoctor(serviceId, userFound.id);
+        queueFound.queue_status = "ON PROCESS";
+        await QueueInternal.save(queueFound);
+        return res.send({
+            status: "Success",
+            message: `Service was successfully assigned to Dr. ${userFound.firstName}`,
         });
     } catch (error) {
         return res.status(401).send({
--- src/route/queue.routes.ts
+++ src/route/queue.routes.ts
@@ -1,8 +1,37 @@
 import { Router } from "express";
-import { clearQueue, viewAllQueue } from "../controller/queue.controller";
-import { checkToken } from "../middleware/auth.middleware";
+import {
+    assignDoctorOnQ,
+    clearAllInternalQ,
+    clearQueue,
+    viewAllInternalQ,
+    viewAllQueue,
+} from "../controller/queue.controller";
+import {
+    adminCheck,
+    checkToken,
+    doctorCheck,
+} from "../middleware/auth.middleware";
 
 export const queueRoutes = Router();
 
 queueRoutes.get("/queue/view-all/", checkToken, viewAllQueue);
-queueRoutes.delete("/queue/clear/", checkToken, clearQueue);
+queueRoutes.get("/queue/internal/view-all", checkToken, viewAllInternalQ);
+queueRoutes.delete(
+    "/queue/clear/",
+    checkToken,
+    adminCheck,
+    adminCheck,
+    clearQueue,
+);
+queueRoutes.delete(
+    "/queue/internal/clear/",
+    checkToken,
+    adminCheck,
+    clearAllInternalQ,
+);
+queueRoutes.put(
+    "/queue/internal/assign-doctor/:queue_id",
+    checkToken,
+    doctorCheck,
+    assignDoctorOnQ,
+);
